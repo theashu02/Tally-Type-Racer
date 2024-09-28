1aef7a71b3c105dc8d84c6111a02c73ca9e61974
Ashutosh Chauhan <ashutoshchauhan1919@gmail.com>
2024-09-29 01:46:53 +0530

    api problem

--- backend/Models/Game.js
+++ backend/Models/Game.js
@@ -1,22 +1,45 @@
 const mongoose = require('mongoose');
 
-const PlayerSchema = new mongoose.Schema({
-    currentWordIndex : {
-        type: Number,
-        default : 0
-    },
-    socketID : {type : String},
-    isPartyLeader : {type : Boolean,default : false},
-    WPM : {type : Number, default: -1},
-    nickName : {type : String}
-});
+// const PlayerSchema = new mongoose.Schema({
+//     currentWordIndex : {
+//         type: Number,
+//         default : 0
+//     },
+//     socketID : {type : String},
+//     isPartyLeader : {type : Boolean,default : false},
+//     WPM : {type : Number, default: -1},
+//     nickName : {type : String}
+// });
 
-const GameSchema = new mongoose.Schema({
-    words : [{type : String}],
-    isOpen : {type : Boolean,default : true},
-    isOver : {type : Boolean,default : false},
-    players : [PlayerSchema],
-    startTime : {type : Number}
-});
+// const GameSchema = new mongoose.Schema({
+//     words : [{type : String}],
+//     isOpen : {type : Boolean,default : true},
+//     isOver : {type : Boolean,default : false},
+//     players : [PlayerSchema],
+//     startTime : {type : Number}
+// });
+
+
+const PlayerSchema = new mongoose.Schema(
+  {
+    currentWordIndex: { type: Number, default: 0 },
+    socketID: { type: String },
+    isPartyLeader: { type: Boolean, default: false },
+    WPM: { type: Number, default: -1 },
+    nickName: { type: String },
+  },
+  { timestamps: true }
+);
+
+const GameSchema = new mongoose.Schema(
+  {
+    words: [{ type: String }],
+    isOpen: { type: Boolean, default: true },
+    isOver: { type: Boolean, default: false },
+    players: [PlayerSchema],
+    startTime: { type: Number },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Game',GameSchema);
